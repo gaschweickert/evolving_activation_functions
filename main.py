19d90e873be42ab4d25e10587b9e870ae01973a6
gaschweickert <schweickertgregory@gmail.com>
2022-01-19 11:36:32 +0100

    final test gasearch c3

--- main.py
+++ main.py
@@ -115,9 +115,9 @@ def load_data(data):
     #data.collect_data_from_file("search_data/13-Jan-2022_00:03:32_GA-search_loss-based_cifar10_G=10_N=50_C=3_m=10_b=5_mode=3_train-epochs=50_number-of-blocks=2.csv")
     #data.collect_data_from_file("search_data/14-Jan-2022_16:36:36_GA-search_loss-based_cifar10_G=15_N=50_C=3_m=10_b=5_mode=3_train-epochs=50_number-of-blocks=2.csv")
     data.collect_data_from_file("search_data/15-Jan-2022_12:12:09_GA-search_loss-based_cifar10_G=15_N=50_C=3_m=10_b=5_mode=3_train-epochs=50_number-of-blocks=2.csv")
-    data.collect_data_from_file("search_data/16-Jan-2022_14:05:33_GA-search_loss-based_cifar10_G=15_N=50_C=1_m=10_b=5_mode=1_train-epochs=50_number-of-blocks=2.csv")
-    data.collect_data_from_file("search_data/18-Jan-2022_10:53:37_Random-search_cifar10_G=15_N=50_C=3_mode=3_train-epochs=50_number-of-blocks=2.csv")
-    data.collect_data_from_file("search_data/18-Jan-2022_19:03:17_Random-search_cifar10_G=15_N=50_C=1_mode=1_train-epochs=50_number-of-blocks=2.csv")
+    #data.collect_data_from_file("search_data/16-Jan-2022_14:05:33_GA-search_loss-based_cifar10_G=15_N=50_C=1_m=10_b=5_mode=1_train-epochs=50_number-of-blocks=2.csv")
+    #data.collect_data_from_file("search_data/18-Jan-2022_10:53:37_Random-search_cifar10_G=15_N=50_C=3_mode=3_train-epochs=50_number-of-blocks=2.csv")
+    #data.collect_data_from_file("search_data/18-Jan-2022_19:03:17_Random-search_cifar10_G=15_N=50_C=1_mode=1_train-epochs=50_number-of-blocks=2.csv")
 
 
 def main():
@@ -141,13 +141,12 @@ def main():
     #random_search(dataset = 'cifar10', generations=15, N=50, C=1, train_epochs=50, mode=1, number_of_blocks=2, verbosity=0, save=True)
     #test_candidate(dataset = 'cifar10', candidate_keys = [['max(x, 0)', 'max(x1, x2)', 'log(abs(x + err))']], k = 1, mode=1, no_blocks=2, no_epochs=200, verbosity=1, save_model=False, visualize=False, tensorboard_log=True)
     
-    test_benchmarks(dataset='cifar10', k=5, no_blocks=2, no_epochs=200, verbosity=1, save_model=False, visualize=False, tensorboard_log=False, save_results=True)
+    #test_benchmarks(dataset='cifar10', k=5, no_blocks=2, no_epochs=200, verbosity=1, save_model=False, visualize=False, tensorboard_log=False, save_results=True)
 
-    """
     data = DATA()
     load_data(data)
     data.convert_and_order()
-    data.plot_gen_vs_accuracy()
+    #data.plot_gen_vs_accuracy()
     data_n_tops = data.get_n_top_candidates(3, verbose=1)
     for i, exp_n_tops in enumerate(data_n_tops):
         filename = data.filenames[i]
@@ -155,8 +154,7 @@ def main():
         dataset = split_name[5] if split_name[3] == 'GA-search' else split_name[4]
         no_blocks = int(split_name[-1][-5])
         mode = int(split_name[-3][-1])
-        test_candidates(filename=filename, candidate_list = exp_n_tops, dataset = dataset, k = 1, mode=mode, no_blocks=no_blocks, no_epochs=2, verbose=1, save_model=False, visualize=True, tensorboard_log=False, save_results=True)
-    """
+        test_candidates(filename=filename, candidate_list = exp_n_tops, dataset = dataset, k = 5, mode=mode, no_blocks=no_blocks, no_epochs=200, verbose=1, save_model=False, visualize=True, tensorboard_log=False, save_results=True)
 
     
 
