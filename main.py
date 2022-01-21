0126cf9b052ffcfee8f3095e45cf936fcbc6f155
gaschweickert <schweickertgregory@gmail.com>
2022-01-21 01:05:39 +0100

    wo

--- main.py
+++ main.py
@@ -122,9 +122,9 @@ def main():
     #random_search(dataset = 'cifar10', generations=15, N=50, C=1, train_epochs=50, mode=1, number_of_blocks=2, verbosity=0, save=True)
     #test_candidate(dataset = 'cifar10', candidate_keys = [['max(x, 0)', 'max(x1, x2)', 'log(abs(x + err))']], k = 1, mode=1, no_blocks=2, no_epochs=200, verbosity=1, save_model=False, visualize=False, tensorboard_log=True)
     
-    test_benchmarks(dataset='cifar100', k=5, no_blocks=2, no_epochs=200, verbosity=1, save_model=False, visualize=False, tensorboard_log=False, save_results=True)
+    #test_benchmarks(dataset='cifar100', k=5, no_blocks=2, no_epochs=200, verbosity=1, save_model=False, visualize=False, tensorboard_log=False, save_results=True)
 
-    """
+
     data = DATA()
     load_data(data)
     data.convert_and_order()
@@ -136,7 +136,6 @@ def main():
         no_blocks = int(split_name[-1][-5])
         mode = int(split_name[-3][-1])
         test_candidates(filename=filename, candidate_list = exp_n_tops, dataset = 'cifar10', k = 5, mode=mode, no_blocks=4, no_epochs=200, verbose=1, save_model=False, visualize=False, tensorboard_log=False, save_results=True)
-    """
 
 if __name__ == "__main__":
     main()
