c7a2cf2f1f859aeb96c1810506ec9a55fc0b7b86
gaschweickert <schweickertgregory@gmail.com>
2022-01-21 01:08:54 +0100

    trans k5 bnn setup

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def main():
         split_name = filename.split("_")
         no_blocks = int(split_name[-1][-5])
         mode = int(split_name[-3][-1])
-        test_candidates(filename=filename, candidate_list = exp_n_tops, dataset = 'cifar10', k = 5, mode=mode, no_blocks=4, no_epochs=200, verbose=1, save_model=False, visualize=False, tensorboard_log=False, save_results=True)
+        test_candidates(filename=filename, candidate_list = exp_n_tops, dataset = 'cifar10', k = 5, mode=mode, no_blocks=4, no_epochs=200, verbose=0, save_model=False, visualize=False, tensorboard_log=False, save_results=True)
 
 if __name__ == "__main__":
     main()
